a4f5f4d660b560b0343d3810151749613283b25e
Phil Welsch <pwelsch86@gmail.com>
2021-12-29 15:48:08 +0100

    missed speed up for odds

--- c8_5.cpp
+++ c8_5.cpp
@@ -26,40 +26,66 @@
 // mul(1, 8) // 8
 
 using u32 = uint32_t;
-using Cache = std::unordered_map<u32, u32>;
+// using Cache = std::unordered_map<u32, u32>;
 
-u32 multiply(u32 sml, u32 lrg, Cache& cache);
+// u32 multiply(u32 sml, u32 lrg, Cache& cache);
 
-u32 recursive_multiply(u32 a, u32 b, Cache& cache) {
+// u32 recursive_multiply(u32 a, u32 b, Cache& cache) {
+//     int sml = a < b ? a : b;
+//     int lrg = a > b ? a : b;
+//     return multiply(sml, lrg, cache); 
+// }
+
+// u32 multiply(u32 sml, u32 lrg, Cache& cache) {
+//     assert(sml <= lrg);
+//     if (sml == 1) { return lrg; }
+    
+//     auto found = cache.find(sml);
+//     if (found != cache.end()) {
+//         return found->second;
+//     }
+
+//     u32 half_sml = sml >> 1;
+//     u32 side1 = recursive_multiply(half_sml, lrg, cache);
+//     u32 total = 0;
+//     if (sml & 1) {
+//         total = side1 + recursive_multiply(sml - half_sml, lrg, cache);
+//     } else {
+//         total = side1 + side1;
+//     }
+
+//     cache.insert({sml, total});
+//     return total;
+// }
+
+// int main(int argc, char** argv) {
+//     Cache cache;
+//     printf("%u\n", recursive_multiply(23, 17, cache));
+//     return 0;
+// }
+
+u32 multiply(u32 sml, u32 lrg);
+
+u32 recursive_multiply(u32 a, u32 b) {
     int sml = a < b ? a : b;
     int lrg = a > b ? a : b;
-    return multiply(sml, lrg, cache); 
+    return multiply(sml, lrg); 
 }
 
-u32 multiply(u32 sml, u32 lrg, Cache& cache) {
+u32 multiply(u32 sml, u32 lrg) {
     assert(sml <= lrg);
     if (sml == 1) { return lrg; }
     
-    auto found = cache.find(sml);
-    if (found != cache.end()) {
-        return found->second;
-    }
-
     u32 half_sml = sml >> 1;
-    u32 side1 = recursive_multiply(half_sml, lrg, cache);
-    u32 total = 0;
+    u32 side1 = recursive_multiply(half_sml, lrg);
+    u32 total = side1 + side1;
     if (sml & 1) {
-        total = side1 + recursive_multiply(sml - half_sml, lrg, cache);
-    } else {
-        total = side1 + side1;
+        total += lrg;
     }
-
-    cache.insert({sml, total});
     return total;
 }
 
 int main(int argc, char** argv) {
-    Cache cache;
-    printf("%u\n", recursive_multiply(23, 17, cache));
+    printf("%u\n", recursive_multiply(23, 17));
     return 0;
 }
